92b87f7916faf49b34715e413c3a9701fe18e75f
Jeremiah <jeremiaht.walters@gmail.com>
2016-08-08 00:04:28 -0700

    added more comments
    added reservation time.

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
-
 if (window.localStorage) {
   var txtEmail = document.getElementById('email');//get form elements
   var txtMyname = document.getElementById('myname');
@@ -39,50 +36,6 @@ if (window.localStorage) {
 
   txtComments.addEventListener('input', function() {
     localStorage.setItem('comments', txtComments.value);
-=======
->>>>>>> gh-pages
-if (window.localStorage) {
-  var txtEmail = document.getElementById('email');
-  var txtmyName = document.getElementById('myName')
-  var txtfirstName = document.getElementById('firstName')
-  var txtlastName = document.getElementById('lastName')
-  var txtreserveEmail = document.getElementById('reserveEmail')
-  var txtcomments = document.getElementById('comments')
 
-  txtEmail.value = localStorage.getItem('email');
-  txtmyName.value = localStorage.getItem('myName')
-  txtfirstName.value = localStorage.getItem('firstName')
-  txtlastName.value = localStorage.getItem('lastName')
-  txtreserveEmail.value = localStorage.getItem('reserveEmail')
-  txtcomments.value = localStorage.getItem('comments')
-
-  txtEmail.addEventListener('input', function(){
-    localStorage.setItem('email', txtEmail.value);
   }, false);
-
-  txtmyName.addEventListener('input', function(){
-    localStorage.setItem('myName', txtmyName.value);
-  }, false);
-
-  txtfirstName.addEventListener('input', function(){
-    localStorage.setItem('firstName', txtfirstName.value);
-  }, false);
-
-  txtlastName.addEventListener('input', function(){
-    localStorage.setItem('lastName', txtlastName.value);
-  }, false);
-
-  txtreserveEmail.addEventListener('input', function(){
-    localStorage.setItem('reserveEmail', txtreserveEmail.value);
-  }, false);
-
-  txtcomments.addEventListener('input', function(){
-    localStorage.setItem('comments', txtcomments.value);
-<<<<<<< HEAD
-=======
->>>>>>> master
->>>>>>> gh-pages
-  }, false);
-
-
 }
