7a54b7980a2766704fb199e30c5cca67f7852b62
Jeremiah <jeremiaht.walters@gmail.com>
2016-08-22 01:12:02 -0700

    Added more constructors
    added constructors to happy hour page and fixed syntax

--- js/models/special.js
+++ js/models/special.js
@@ -1,11 +1,12 @@
-var Special = function(title, price) {
+var Special = function(title, price, recipe) {
   this.title = title,
-  this.price = price;
+  this.price = price,
+  this.recipe = recipe;
 }
 
 /**
 * Returns the formatted special text
 */
 Special.prototype.getMessage = function(){
-  return "Today is " + this.title + " for $" + this.price;
+  return "Today is " + this.title + " for $" + this.price + " Includes " + this.recipe;
 }
